fa94485900cb9a2013b5465b9fb35dc283a26676
Mathias Rav <rav@cs.au.dk>
2016-03-18 19:44:48 +0100

    Support multiple files

--- mutag.py
+++ mutag.py
@@ -12,37 +12,54 @@ import mutagen.easyid3
 import mutagen.easymp4
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-b', '--backup', dest='backup',
-                        action='store_true')
-    parser.add_argument('filename')
-    args = parser.parse_args()
-
-    keys = 'artist album title tracknumber discnumber date'.split()
-
-    ext = os.path.splitext(args.filename)[1]
+def load_file(filename, parser, args):
+    ext = os.path.splitext(filename)[1]
     if ext == '.mp3':
-        t = mutagen.easyid3.EasyID3(args.filename)
+        t = mutagen.easyid3.EasyID3(filename)
     elif ext == '.m4a':
-        t = mutagen.easymp4.EasyMP4(args.filename)
+        t = mutagen.easymp4.EasyMP4(filename)
     else:
         parser.error("Unknown extension %s" % shlex.quote(ext))
 
+    keys = args.keys
     tags = collections.OrderedDict([(k, '') for k in keys])
     for k, v in t.items():
         tags[k] = ', '.join(v)
 
+    return filename, (t, tags)
+
+
+def escape(v):
+    return ' '.join(shlex.quote(s) for s in v.split())
+
+
+def main():
+    def_keys = 'artist album title tracknumber discnumber date'.split()
+    short_keys = 'artist title'.split()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--backup', dest='backup',
+                        action='store_true')
+    parser.add_argument('-s', '--short', dest='keys',
+                        action='store_const', const=short_keys,
+                        default=def_keys)
+    parser.add_argument('filename', nargs='+')
+    args = parser.parse_args()
+
+    files = collections.OrderedDict(
+        [load_file(filename, parser, args)
+         for filename in args.filename])
+
     with tempfile.NamedTemporaryFile(mode='w+') as fp:
-        for k, v in tags.items():
-            fp.write('%s ' % k)
-            fp.write(' '.join(shlex.quote(s) for s in v.split()))
+        for filename, (tagger, tags) in files.items():
+            fp.write('filename %s\n' % (escape(filename),))
+            for k, v in tags.items():
+                fp.write('%s %s\n' % (k, escape(v)))
             fp.write('\n')
         fp.write(textwrap.dedent("""\
         # Each line will be written to the file
         # Deleting a line will leave the tag unchanged
-        # Original filename was %s
-        """ % shlex.quote(args.filename)))
+        """))
         fp.flush()
         returncode = subprocess.call(('vim', fp.name))
         if returncode != 0:
@@ -50,34 +67,43 @@ def main():
                 "Editor returned %s; quitting" % returncode)
         fp.seek(0)
         s = fp.read()
-    lines = 0
+    lines = {filename: 0 for filename in files.keys()}
+    current_file = None
     for l in s.splitlines():
         if l.startswith('#'):
             continue
         p = shlex.split(l)
+        if not p:
+            continue
         key = p[0]
         if key.startswith('#'):
             continue
-        lines += 1
         value = ' '.join(p[1:])
+        if key == 'filename':
+            current_file = value
+            continue
+        lines[current_file] += 1
         if not value:
-            t.pop(key, None)
+            files[current_file][0].pop(key, None)
         else:
-            t[key] = [value]
-    if not lines:
-        raise SystemExit(
-            "No lines in buffer; quitting")
-    if args.backup:
-        backupname = args.filename + '~'
-        if os.path.exists(backupname):
-            print("Not overwriting existing backup %s" %
-                  shlex.quote(backupname))
-        else:
-            print("Copying file to %s" % shlex.quote(backupname))
-            shutil.copyfile(args.filename, backupname)
-            shutil.copystat(args.filename, backupname)
-    print("Saving file with mutagen")
-    t.save()
+            files[current_file][0][key] = [value]
+
+    for filename, (t, tags) in files.items():
+        if not lines[filename]:
+            print("%s: No lines in buffer" % shlex.quote(filename))
+            continue
+        if args.backup:
+            backupname = filename + '~'
+            if os.path.exists(backupname):
+                print("%s: Not overwriting existing backup %s" %
+                      (shlex.quote(filename), shlex.quote(backupname)))
+            else:
+                print("Copying %s to %s" %
+                      (shlex.quote(filename), shlex.quote(backupname)))
+                shutil.copyfile(filename, backupname)
+                shutil.copystat(filename, backupname)
+        print("Saving %s with mutagen" % shlex.quote(filename))
+        t.save()
 
 
 if __name__ == "__main__":
